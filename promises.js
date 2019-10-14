dee17dacc63f215c75ca7e830eb3b82b1ccc44f3
robooze <rob.genchi@gmail.com>
2019-10-14 13:41:14 +0200

    added async / await

--- promises.js
+++ promises.js
@@ -37,13 +37,36 @@ function createPost(post) {
 //     .then(getPosts)  // if the result of createPost promise is resolve THEN do this.
 //     .catch(err => console.log(err));  // if the result of createPost promise is reject, print the reject error defined above
 
-// Promise.all
-const promise1 = Promise.resolve("I'm promise 1");
-const promise2 = "I'm promise 2";
-const promise3 = new Promise((resolve, reject)=> setTimeout(resolve, 2000, "Goodbye"));
-// fetch API returns a promise
-const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json() );
 
-Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
+// Async / Await
+// async function init() {
+//     // let's wait for an asynchronous action to end:
+//     await createPost({ title: "Post Three", body: "This is the third post" });
+//
+//     // First we wait createPost to have a response, then: ... it's just a different syntax
+//     getPosts();
+// }
+//
+// init();
 
-console.log("I'm coded after a Promise. Let's see if we work asynchronously!");
+// Async / Await with fetch from FETCH API
+async function fetchUsers() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    const data = await res.json();
+
+    console.log(data);
+}
+
+fetchUsers();
+
+// // Promise.all
+// const promise1 = Promise.resolve("I'm promise 1");
+// const promise2 = "I'm promise 2";
+// const promise3 = new Promise((resolve, reject)=> setTimeout(resolve, 2000, "Goodbye"));
+// // fetch API returns a promise
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json() );
+//
+// Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
+//
+// console.log("I'm coded after a Promise. Let's see if we work asynchronously!");
